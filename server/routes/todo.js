8a12c0d0c9802fe7554442ad899664ee8e444253
Pulkit Narula <43206958+pulkitnarula13@users.noreply.github.com>
2022-12-10 21:53:11 -0800

    routes of todo

---
+++ server/routes/todo.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+    addTodo,
+    getTodo,
+    getTodoById,
+    deleteTodo,
+    updateTodo
+} = require("../controller/todo");
+
+// const validateToken = require("../middleware/auth");
+
+// Routes
+/**
+ * @swagger
+ * /register:
+ *   post:
+ *     description: Add a new Todo
+ *     responses:
+ *       200:
+ *         description: return positive response
+ */
+router.post("/todo", addTodo);
+
+
+// Routes
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     description: Get all the Todos
+ *     responses:
+ *       200:
+ *         description: return positive response
+ */
+router.get("/", Todos);
+
+// Routes
+/**
+ * @swagger
+ * /todo/:id:
+ *   get:
+ *     description: Get information of a Todo using id
+ *     responses:
+ *       200:
+ *         description: return positive response
+ */
+router.get("/:id", getTodoById);
+
+// Routes
+/**
+ * @swagger
+ * /todo/:id:
+ *   put:
+ *     description: Update information of a Todo
+ *     responses:
+ *       200:
+ *         description: return positive response
+ */
+router.put("/:id", updateTodo);
+
+
+// Routes
+/**
+ * @swagger
+ * /doctor/:id:
+ *   delete:
+ *     description: Delete a Todo using id from the database
+ *     responses:
+ *       200:
+ *         description: return positive response
+ */
+router.delete("/:id", deleteTodo);
+
+
+module.exports = router;
