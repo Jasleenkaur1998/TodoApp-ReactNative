a7737b00e12ed54b96c5dc8fa90de56a10670b35
KapilThaman <92117060+KapilThaman@users.noreply.github.com>
2022-12-10 22:00:42 -0800

    swagger setup

--- server/server.js
+++ server/server.js
@@ -2,7 +2,30 @@ const express = require("express");
 const app = express();
 const PORT  = 8080;
 const mongoose = require("mongoose");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 require('dotenv').config();
+
+
+// swagger
+const swaggerOptions = {
+    swaggerDefinition: {
+      info: {
+        title: "TODO API",
+        description: "TODO API Information",
+        contact: {
+          name: "TODO Developers",
+        },
+        servers: ["http://localhost:8080"],
+      },
+    },
+    apis: ["./routes/*.js", "server.js"],
+    // apis: ["app.js"]
+  };
+  
+  const swaggerDocs = swaggerJsDoc(swaggerOptions);
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+  
 
 // const todoRoutes = require("./routes/todo");
 // const userRoutes = require("./routes/user");
@@ -20,7 +43,15 @@ mongoose.connect(process.env.MONGO_URI, (error) => {
 
 // app.use("/api/v1/todos", todoRoutes);
 
-
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     description: Welcome to swagger-jsdoc!
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious string.
+ */
 app.get("/", (request, response) => {
     response.send("This is todo endpoint!");
 })
